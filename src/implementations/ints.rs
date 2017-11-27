e3709d4a1080dded66b92d032e290fe9992e7b59
Markus Ongyerth <ongy@ongy.net>
2017-11-27 20:37:22 +0100

    Add u32 instance

--- src/implementations/ints.rs
+++ src/implementations/ints.rs
@@ -39,6 +39,43 @@ impl ConfigAble for i32 {
     fn merge(&mut self, rhs: Self) -> Result<(), ()> { if *self == rhs { Ok(()) } else { Err(()) } }
 }
 
+impl ConfigAble for u32 {
+    fn get_format<F>(_: &mut std::collections::HashSet<String>, fun: &mut F) 
+        where F: FnMut(&str) {
+            fun("u32: Digits");
+        }
+
+    fn get_name() -> &'static str { "u32" }
+
+    fn parse_from<F>(provider: &mut ConfigProvider, fun: &mut F) -> Result<Self, ParseError>
+        where F: FnMut(String) {
+
+        if let Some(content) = provider.get_next() {
+            /* Fix this, it's majorly wrong */
+            let max = content.find(|c: char| c.is_whitespace()).unwrap_or(content.len());
+            let tmp = &content[0 .. max];
+            match tmp.parse::<u32>() {
+                Ok(ret) => {
+                    provider.consume(max, fun)?;
+                    return Ok(ret);
+                },
+                Err(x) => {
+                    fun(format!("Failed to parse '{}' into an u32: {}", tmp, x));
+                    return Err(ParseError::Recoverable);
+                }
+            }
+        }
+
+        fun("At end of file :(".to_string());
+        return Err(ParseError::Final);
+    }
+
+    fn get_default() -> Result<Self, ()> { Err(()) }
+
+    fn merge(&mut self, rhs: Self) -> Result<(), ()> { if *self == rhs { Ok(()) } else { Err(()) } }
+}
+
+
 impl ConfigAble for u8 {
     fn get_format<F>(_: &mut std::collections::HashSet<String>, fun: &mut F) 
         where F: FnMut(&str) {
