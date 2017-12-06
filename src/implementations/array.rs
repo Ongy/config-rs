4527e8a5a2d952fe2713096ab9b3e64c636f0e51
Markus Ongyerth <ongy@ongy.net>
2017-12-06 11:43:39 +0100

    Add implementation for [T;6], these should be macros... and sane

--- src/implementations/array.rs
+++ src/implementations/array.rs
@@ -34,14 +34,37 @@ impl<T: ConfigAble> ConfigAble for [T;4] {
 
     fn merge(&mut self, _: Self) -> Result<(), ()> {
         return Err(());
-//        unsafe {
-//            let ptr = rhs.as_ptr();
-//
-//            self[0].merge(ptr.offset(0).read())?;
-//            self[1].merge(ptr.offset(1).read())?;
-//            self[2].merge(ptr.offset(2).read())?;
-//            self[3].merge(ptr.offset(3).read())?;
-//        }
-//        Ok(())
+    }
+}
+
+impl<T: ConfigAble> ConfigAble for [T;6] {
+    fn get_format<F>(_: &mut std::collections::HashSet<String>, fun: &mut F) 
+        where F: FnMut(&str) {
+            fun("[T;4]: [T, T, T, T, T, T]");
+        }
+
+    fn get_name() -> &'static str { "[T;6]" }
+
+    fn parse_from<F>(provider: &mut ConfigProvider, fun: &mut F) -> Result<Self, ParseError>
+        where F: FnMut(String) {
+        let vec: Vec<T> = ConfigAble::parse_from(provider, fun)?;
+
+        if vec.len() != 6 {
+            fun(format!("Expected array of size 6, got array of size: {}", vec.len()));
+            return Err(ParseError::Recoverable);
+        }
+
+        let mut iter = vec.into_iter();
+
+        let ret = [iter.next().unwrap(), iter.next().unwrap(), iter.next().unwrap(), iter.next().unwrap(), iter.next().unwrap(), iter.next().unwrap()];
+        return Ok(ret);
+    }
+
+    fn get_default() -> Result<Self, ()> {
+        Ok([T::get_default()?, T::get_default()?, T::get_default()?, T::get_default()?, T::get_default()?, T::get_default()?])
+    }
+
+    fn merge(&mut self, _: Self) -> Result<(), ()> {
+        return Err(());
     }
 }
